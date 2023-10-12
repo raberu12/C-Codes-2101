1361965719625ef9053af138b02a2f171b80cbf9
Matt <11400071@usc.edu.ph>
2023-10-12 20:50:06 +0800

    moved comp word out of sets

--- Dictionary/Magdadaro-Closed-Hashing.c
+++ Dictionary/Magdadaro-Closed-Hashing.c
@@ -23,14 +23,19 @@ int hash(unsigned int x);
 Boolean member(Dictionary D, unsigned int elem);
 void insert(Dictionary D, studType S);
 void deleteElem(Dictionary D, studType S);
+void display(Dictionary D);
 
 
 int main(){
 	Dictionary D;
 	studType s1 = {11400071, "Matt Vincent", "Magdadaro", 'E', 2};
+	studType s2 = {22400071, "Shawn Ryan", "Nacario", 'H', 3};
 	init(D);
 	insert(D, s1);
+	insert(D, s2);
 	member(D, 11400071);
+	deleteElem(D, s2);
+	display(D);
 }
 
 void init(Dictionary D){
@@ -67,6 +72,15 @@ void insert(Dictionary D, studType S){
 void deleteElem(Dictionary D, studType S){
 	int hashvalue = hash(S.idNumber);
 	int count = 0;
-	for(; D[hashvalue].idNumber != EMPTY; hashvalue = (hashvalue + 1) % SIZE, count++){}
+	for(; D[hashvalue].idNumber != EMPTY && D[hashvalue].idNumber != S.idNumber; hashvalue = (hashvalue + 1) % SIZE, count++){}
 	D[hashvalue].idNumber = DELETED;
+}
+
+void display(Dictionary D){
+	int i = 0;
+	for(i = 0; i < SIZE; i++){
+		if(D[i].idNumber != EMPTY && D[i].idNumber != DELETED){
+			printf("ID: %d\nName: %s %c. %s\nYear Level: %d\n", D[i].idNumber, D[i].FN, D[i].MI, D[i].LN, D[i].yearLevel);
+		}
+	}
 }
